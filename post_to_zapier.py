08284ad385980c5cb61474b174f1e59cfc596630
khoslarhea-hub <khoslarhea@gmail.com>
2026-08-19 22:49:43 -0700

    Update post_to_zapier.py

--- post_to_zapier.py
+++ post_to_zapier.py
@@ -1,7 +1,13 @@
-import os, requests
+import os, re, requests
 
 with open("review.txt") as f:
     review = f.read()
+
+# Convert Markdown headers (##, ###) into Slack bold
+review = re.sub(r'^#{1,6}\s*(.+)$', r'*\1*', review, flags=re.MULTILINE)
+
+# Convert **bold** into Slack's *bold*
+review = re.sub(r'\*\*(.+?)\*\*', r'*\1*', review)
 
 requests.post(
     os.environ["ZAPIER_WEBHOOK_URL"],
